4051aadabcf83b523cbfe0f919babe1afe6a61a1
aksha1 <akshayag2020@email.iimcal.ac.in>
2020-07-18 13:06:01 +0530

    commit1
    First code

---
+++ file1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 18 12:57:18 2020
+
+@author: aksha
+"""
+# Python3 program to add two numbers 
+
+num1 = 15
+num2 = 12
+
+# Adding two nos 
+sum = num1 + num2 
+
+# printing values 
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum)) 
+
+
